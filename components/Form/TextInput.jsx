841ec697c5d222db8a05cb8242bd85d404b80f6c
SemzyTheMan <semzy2002@gmail.com>
2023-10-02 16:47:04 +0100

    operator

---
+++ components/Form/TextInput.jsx
@@ -0,0 +1,16 @@
+import styles from "./Input.module.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+export const TextInput = ({ label, className }) => {
+  return (
+    <div
+      className={`${montserrat.className} flex flex-col w-[100%] my-[10px] relative ${className}`}
+    >
+      <label className="bg-white left-[10px] absolute top-[-12px]" htmlFor="">
+        {label}
+      </label>
+      <input className={`${styles.Input} w-[100%] h-[40px]`} type="text" />
+    </div>
+  );
+};
---
+++ src/app/test/page.js
@@ -0,0 +1,9 @@
+const index = () => {
+  return (
+    <div>
+      <p className="text-black">hpuuihupui0pbup9</p>
+    </div>
+  );
+};
+
+export default index;
