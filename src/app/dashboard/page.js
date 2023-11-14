d441269c5456f79004819cb0df88e50da5c5f650
digvijay-md <digvijay.bind@mktdynamics.com>
2023-11-14 14:00:56 +0530

    chnages

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,21 +1,21 @@
-import Landing from "../../../components/Landing/page";
-import Modal from "../../../components/Modal";
-import Nav from "../../../components/Nav/nav";
+// import Landing from "../../../components/Landing/page";
+// import Modal from "../../../components/Modal";
+// import Nav from "../../../components/Nav/nav";
 
-import UpdateModal from "../../../components/UpdateModal/page";
-// import Top from "../../../components/pagination/Nav/top";
-import Pagin from "../../../components/pagination/pagin";
+// import UpdateModal from "../../../components/UpdateModal/page";
+// // import Top from "../../../components/pagination/Nav/top";
+// import Pagin from "../../../components/pagination/pagin";
 
 
 const Dashboard = () => {
   return (
     <main>
       {/* <Top /> */}
-      <Nav />
+      {/* <Nav />
       <Landing />
       <Modal />
       <UpdateModal />
-      <Pagin />
+      <Pagin /> */}
     </main>
   );
 };
