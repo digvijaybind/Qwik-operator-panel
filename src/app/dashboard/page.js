96f9c864f50d6de2d864da3d11eab5961f6f4f0e
digvijay-md <digvijay.bind@mktdynamics.com>
2023-11-14 13:43:47 +0530

    chnageschnages

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,19 +1,21 @@
 import Landing from "../../../components/Landing/page";
 import Modal from "../../../components/Modal";
 import Nav from "../../../components/Nav/nav";
-import Top from "../../../components/Nav/top";
+
 import UpdateModal from "../../../components/UpdateModal/page";
+import Top from "../../../components/pagination/Nav/top";
 import Pagin from "../../../components/pagination/pagin";
+
 
 const Dashboard = () => {
   return (
     <main>
-      <Top></Top>
-      <Nav></Nav>
-      <Landing></Landing>
-      <Modal></Modal>
-      <UpdateModal></UpdateModal>
-      <Pagin></Pagin>
+      <Top />
+      <Nav />
+      <Landing />
+      <Modal />
+      <UpdateModal />
+      <Pagin />
     </main>
   );
 };
